b6ac4060bc62915a768546c5bdb56b7ae419eba3
rezafaraji <faraji.reza@outlook.com>
2025-07-17 06:07:01 -0400

    feat: Implement initial CoinsListViewModel for fetching and displaying coins
    - Introduced `CoinsListViewModel` to manage the state and logic for the coins list screen.
    - Injected `GetCoinsListUseCase` to fetch coin data.
    - Implemented `getAllCoins()` suspend function to fetch coin data and update the UI state.
      - On successful data retrieval, maps `CoinItem` domain models to `UiCoinListItem` presentation models and updates the `_state` Flow.
      - Placeholder error handling for `Result.Error` cases (marked with `// TODO`).
    - Utilizes `MutableStateFlow` (`_state`) to hold the `CoinsState`.
    - Exposes `state` as a `StateFlow` which is initialized by calling `getAllCoins()` and configured with `SharingStarted.WhileSubscribed`.

---
+++ composeApp/src/commonMain/kotlin/dev/reza/cmptrading/coins/presentation/CoinsListViewModel.kt
@@ -0,0 +1,59 @@
+package dev.reza.cmptrading.coins.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.reza.cmptrading.coins.domain.usecase.GetCoinDetailUseCase
+import dev.reza.cmptrading.coins.domain.usecase.GetCoinPriceHistoryUseCase
+import dev.reza.cmptrading.coins.domain.usecase.GetCoinsListUseCase
+import dev.reza.cmptrading.core.domain.Result
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class CoinsListViewModel(
+    private val getCoinsListUseCase: GetCoinsListUseCase
+): ViewModel()  {
+
+    private val _state = MutableStateFlow(CoinsState())
+    val state = _state
+        .onStart { getAllCoins() }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = CoinsState()
+        )
+
+    private suspend fun getAllCoins() {
+        when(val coinsResponse  = getCoinsListUseCase.execute()) {
+            is Result.Success -> {
+                _state.update {
+                    CoinsState(
+                        coins = coinsResponse.data.map { coinItem ->
+                            UiCoinListItem(
+                                id = coinItem.coin.id,
+                                symbol = coinItem.coin.symbol,
+                                name = coinItem.coin.name,
+                                iconUrl = coinItem.coin.iconUrl,
+                                formattedPrice = coinItem.price.toString(),
+                                formattedChange = coinItem.change.toString(),
+                                isPossible = coinItem.change >= 0
+                            )
+                        }
+                    )
+                }
+            }
+            is Result.Error -> {
+                _state.update {
+                    CoinsState(
+                        error = null // TODO
+                    )
+                }
+
+            }
+        }
+    }
+
+}
